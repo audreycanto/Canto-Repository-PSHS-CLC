a7298b01213e82b553f0a3c7a5f3a950d616e157
audreycanto <louiseaudreanacanto2@gmail.com>
2026-08-27 09:22:08 +0800

    Update q1_sg5_a1_arayat_Canto.py

--- q1/q1_sg5_a1_arayat_Canto.py
+++ q1/q1_sg5_a1_arayat_Canto.py
@@ -1,1 +1,13 @@
 
+class Hero:
+    def __init__(self, name, hp=100):
+        self.name = name
+        self.hp = hp
+    def take_damage(self, amount):
+        self.hp = self.hp - amount
+Arthur = Hero(Arthur, 100)
+Morgana = Hero(Morgana, 100)
+Arthur.takedamage(10)
+print("Arthur's hp", self.hp)
+print("Morgana's hp", self.hp)
+        
